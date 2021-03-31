9c2f292ed5ec4bccdce6bafa5e88154cffa569d2
Rodrigo R <pmello.rodrigor@gmail.com>
2021-03-30 22:09:51 -0300

    adding some experiments

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/adrg/strutil"
+	"github.com/adrg/strutil/metrics"
 	"github.com/gocolly/colly/v2"
 	"github.com/montanaflynn/stats"
 )
@@ -59,13 +61,10 @@ func main() {
 	// the returned list and enter in the links
 	// to retrieved the desired information
 	c.OnHTML(`a.ui-search-item__group__element.ui-search-link`, func(e *colly.HTMLElement) {
-
-		log.Println("Starting crawler")
+		log.Println("Starting Scraper")
 		productLink := e.Attr("href")
-		//e.Request.Visit(productLink)
 		log.Println("Visiting item: ", productLink)
 		detailCollector.Visit(productLink)
-
 	})
 
 	detailCollector.OnHTML("div.ui-pdp-container__row.ui-pdp-component-list.pr-16.pl-16", func(e *colly.HTMLElement) {
@@ -135,10 +134,37 @@ func main() {
 
 }
 
-func removeOutliers(itens *[]Item) []Item {
+func findSimilarities(items []Item) {
+	for i := 0; i < len(items)/2; i++ {
+		for j := 1; j < len(items)/2; j++ {
+			similarity := strutil.Similarity(items[i].Title, items[j].Title, metrics.NewHamming())
+			log.Println("Similarity ", items[i].Title, items[j].Title, similarity)
+		}
+	}
+}
+
+func extractPrices(items *[]Item) []float64 {
+	data := []float64{}
+	for _, v := range *items {
+		data = append(data, v.Price)
+	}
+
+	return data
+}
+
+func findQuartile(items *[]Item) (stats.Outliers, error) {
+	prices := extractPrices(items)
+	q, err := stats.QuartileOutliers(prices)
+	if err != nil {
+		return stats.Outliers{}, err
+	}
+	return q, nil
+}
+
+func removeOutliers(items *[]Item) []Item {
 
 	data := []float64{}
-	for _, v := range *itens {
+	for _, v := range *items {
 		data = append(data, v.Price)
 	}
 
@@ -148,7 +174,7 @@ func removeOutliers(itens *[]Item) []Item {
 	q, _ := stats.Quartile(data)
 	log.Println(q)
 	cleanedData := []Item{}
-	for _, v := range *itens {
+	for _, v := range *items {
 		if math.Abs(v.Price) > math.Abs(mean-threshold*std) && math.Abs(v.Price) < math.Abs(mean+threshold*std) {
 			cleanedData = append(cleanedData, v)
 		}
